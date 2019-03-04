66a4a46f3685b3c1416fb065c3c49ac72adaa2ba
Benjamin Spriggs <ben@sprico.com>
2019-03-03 20:50:07 -0800

    Remove testing code

--- app/src/config.ts
+++ app/src/config.ts
@@ -2,23 +2,10 @@ import { isPackaged, applicationRoot } from "ibis-lib"
 import { join } from "path"
 import { readdir } from "fs"
 
-function t(pre: string, path: string) {
-    console.log('testing:', pre, path)
-    readdir(path, (err, items) => {
-        if (err) console.error(err)
-        console.log(pre, "#", path, ":", items)
-    })
-}
-
 const root = isPackaged() ? applicationRoot : join(applicationRoot, "app/dist/")
 const views = join(root, "views")
 const semantic = join(root, "semantic/dist")
 const _public = join(root, "public")
-
-t("root", root)
-t("views", views)
-t("semantic", semantic)
-t("public", _public)
 
 export const paths = {
     root,

4281985eb1d659bfc85b0f6c7a742c1644d0305f
Benjamin Spriggs <ben@sprico.com>
2019-05-06 05:40:08 -0700

    Revert "Use UMD instead"
    This reverts commit 77769e1de91a20e736823055a8d5da22f197d495.

--- app/dist/public/scripts/app.js
+++ app/dist/public/scripts/app.js
@@ -1,64 +1,52 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+import "./semantic-api";
+function formatBackendResource(url) {
+    const u = new URL(url);
+    const [_, first, ...rest] = u.pathname.split("/");
+    if (first === "rx") {
+        return `/materia-medica/${rest.join("/")}`;
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./semantic-api"], factory);
+    else if (first == "tx") {
+        return `/therapeutics/${rest.join("/")}`;
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    require("./semantic-api");
-    function formatBackendResource(url) {
-        const u = new URL(url);
-        const [_, first, ...rest] = u.pathname.split("/");
-        if (first === "rx") {
-            return `/materia-medica/${rest.join("/")}`;
-        }
-        else if (first == "tx") {
-            return `/therapeutics/${rest.join("/")}`;
-        }
-        else {
-            throw new Error("unknown: " + first);
-        }
+    else {
+        throw new Error("unknown: " + first);
     }
-    $(document).ready(() => {
-        $("#modality-menu-toggle").click(() => {
-            let menu = $("#modality-menu");
-            if (menu) {
-                menu.sidebar("toggle");
+}
+$(document).ready(() => {
+    $("#modality-menu-toggle").click(() => {
+        let menu = $("#modality-menu");
+        if (menu) {
+            menu.sidebar("toggle");
+        }
+    });
+    $(".ui.button.htm-link").api({
+        encodeParameters: false,
+        onSuccess: (data) => {
+            console.dir(data);
+        }
+    });
+    $(".ui.search").search({
+        apiSettings: {
+            onResponse: (data) => {
+                return (Object.assign({}, data, { results: data.results.map(result => (Object.assign({}, result, { category: result.modality.data.displayName, title: result.header.name, url: formatBackendResource(result.url) }))) }));
             }
-        });
-        $(".ui.button.htm-link").api({
-            encodeParameters: false,
-            onSuccess: (data) => {
-                console.dir(data);
+        }
+    });
+    $(".ui.search.categorize").search({
+        type: "category",
+        apiSettings: {
+            onResponse: (data) => {
+                return (Object.assign({}, data, { results: Object.keys(data.results).reduce((acc, category) => {
+                        const d = data.results[category];
+                        acc[category] = (Object.assign({}, d, { results: d.results.map(result => ({
+                                category: result.modality.data.displayName,
+                                title: result.header.name,
+                                url: formatBackendResource(result.url)
+                            })) }));
+                        return acc;
+                    }, {}) }));
             }
-        });
-        $(".ui.search").search({
-            apiSettings: {
-                onResponse: (data) => {
-                    return (Object.assign({}, data, { results: data.results.map(result => (Object.assign({}, result, { category: result.modality.data.displayName, title: result.header.name, url: formatBackendResource(result.url) }))) }));
-                }
-            }
-        });
-        $(".ui.search.categorize").search({
-            type: "category",
-            apiSettings: {
-                onResponse: (data) => {
-                    return (Object.assign({}, data, { results: Object.keys(data.results).reduce((acc, category) => {
-                            const d = data.results[category];
-                            acc[category] = (Object.assign({}, d, { results: d.results.map(result => ({
-                                    category: result.modality.data.displayName,
-                                    title: result.header.name,
-                                    url: formatBackendResource(result.url)
-                                })) }));
-                            return acc;
-                        }, {}) }));
-                }
-            }
-        });
+        }
     });
 });
 
--- app/dist/public/scripts/semantic-api.js
+++ app/dist/public/scripts/semantic-api.js
@@ -1,22 +1,10 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "ibis-api/dist/config"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    //@ts-check
-    const config_1 = require("ibis-api/dist/config");
-    $.api.settings.verbose = true;
-    $.api.settings.api = {
-        'search treatments': `${config_1.apiHostname}/data/treatments?q={query}`,
-        'search diseases': `${config_1.apiHostname}/data/diseases?q={query}&categorize=true`,
-        'search': `${config_1.apiHostname}/data?q={query}`
-    };
-});
+//@ts-check
+import { apiHostname } from "ibis-api/dist/config";
+$.api.settings.verbose = true;
+$.api.settings.api = {
+    'search treatments': `${apiHostname}/data/treatments?q={query}`,
+    'search diseases': `${apiHostname}/data/diseases?q={query}&categorize=true`,
+    'search': `${apiHostname}/data?q={query}`
+};
 
 //# sourceMappingURL=semantic-api.js.map

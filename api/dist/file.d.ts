1c2e974463efb718d8a25be73e96eb1a93b1c958
bspriggs <ben@sprico.com>
2019-05-11 12:59:50 -0700

    Remove dist

--- api/dist/file.d.ts
+++
@@ -1,8 +0,0 @@
-import { Header } from "ibis-lib";
-export declare function getFileInfo(absoluteFilePath: string, modality: string, listing: string[]): Promise<{
-    filename: string;
-    header: Header;
-}[]>;
-export declare const getListing: (absoluteFilePath: string, modality: string) => Promise<string[]>;
-declare const _default;
-export default _default;

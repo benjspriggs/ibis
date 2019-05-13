be65f6c994f2d388d0ae848b5a8e59e6b8b896b1
bspriggs <ben@sprico.com>
2019-05-13 09:05:57 -0700

    If a key and cert are provided, use those

--- lib/src/server.ts
+++ lib/src/server.ts
@@ -3,18 +3,22 @@ import pem from "pem"
 import { IncomingMessage, ServerResponse } from "http"
 
 export const createServer = (app: (request: IncomingMessage, response: ServerResponse) => void, options?: spdy.ServerOptions) => new Promise<spdy.Server>((resolve, reject) => {
-    pem.createCertificate({
-        days: 1,
-        selfSigned: true
-    }, (err, keys) => {
-        if (err) {
-            reject(err)
-        } else {
-            const serverOptions: spdy.ServerOptions = Object.assign({
-                key: keys.serviceKey,
-                cert: keys.certificate
-            }, options)
-            resolve(spdy.createServer(serverOptions, app))
-        }
-    })
+    if (options && options.key && options.cert) {
+        resolve(spdy.createServer(options, app))
+    } else {
+        pem.createCertificate({
+            days: 1,
+            selfSigned: true
+        }, (err, keys) => {
+            if (err) {
+                reject(err)
+            } else {
+                const serverOptions: spdy.ServerOptions = Object.assign({
+                    key: keys.serviceKey,
+                    cert: keys.certificate
+                }, options)
+                resolve(spdy.createServer(serverOptions, app))
+            }
+        })
+    }
 })

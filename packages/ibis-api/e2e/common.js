590558828a9ba753e377cd3f7dda848c56ecd530
Benjamin Spriggs <ben@sprico.com>
2019-06-15 16:47:38 -0700

    Add random port helper

--- packages/ibis-api/e2e/common.js
+++ packages/ibis-api/e2e/common.js
@@ -4,13 +4,17 @@ const test = require("ava");
 const { get } = require("request");
 
 module.exports = function(options) {
-    const port = 8080 + Math.floor(Math.random() * 100)
+    const randomPort = (t, run) => {
+        const port = 3000 + Math.floor(Math.random() * 100)
 
-    process.env.API_PORT = port.toString();
+        process.env.API_PORT = port.toString();
+
+        return run(t, port)
+    }
 
     const withApi = withEntrypoint(options)
 
-    test("It should serve a 200 for root", withApi, async (t) => {
+    test("It should serve a 200 for root", randomPort, withApi, async (t, port) => {
         await new Promise((resolve, reject) => {
             get(`http://localhost:${port}`)
                 .on('response', (response) => {
--- packages/ibis-app/e2e/common.js
+++ packages/ibis-app/e2e/common.js
@@ -3,8 +3,8 @@ const { withEntrypoint } = require("ibis-lib");
 const test = require("ava");
 const { get } = require("request");
 
-const fetchAndOk = (url) => (t) => new Promise((resolve, reject) => {
-            get(url)
+const fetchAndOk = (url) => (t, port) => new Promise((resolve, reject) => {
+            get(url(port))
                 .on('response', (response) => {
                     t.is(200, response.statusCode)
                     t.truthy(response.headers)
@@ -13,8 +13,8 @@ const fetchAndOk = (url) => (t) => new Promise((resolve, reject) => {
                 .on('error', reject)
         });
 
-const fetchAndNotOk = (url) => (t) => new Promise((resolve, reject) => {
-            get(url)
+const fetchAndNotOk = (url) => (t, port) => new Promise((resolve, reject) => {
+            get(url(port))
                 .on('response', (response) => {
                     t.not(200, response.statusCode)
                     t.truthy(response.headers)
@@ -24,19 +24,23 @@ const fetchAndNotOk = (url) => (t) => new Promise((resolve, reject) => {
         });
 
 module.exports = function(options) {
-    const port = 8080 + Math.floor(Math.random() * 100)
+    const randomPort = (t, run) => {
+        const port = 8080 + Math.floor(Math.random() * 100)
 
-    process.env.APP_PORT = port.toString();
+        process.env.APP_PORT = port.toString();
+
+        return run(t, port)
+    }
 
     const withApp = withEntrypoint(options)
 
-    test("It should serve a 200 for root", withApp, fetchAndOk(`http://localhost:${port}`))
+    test("It should serve a 200 for root", randomPort, withApp, fetchAndOk(port => `http://localhost:${port}`))
 
-    test("It should serve JS from the static path", withApp, fetchAndOk(`http://localhost:${port}/assets/scripts/app.js`))
+    test("It should serve JS from the static path", randomPort, withApp, fetchAndOk(port => `http://localhost:${port}/assets/scripts/app.js`))
 
-    test("It should serve CSS from the static path",  withApp, fetchAndOk(`http://localhost:${port}/assets/semantic/semantic.min.css`))
+    test("It should serve CSS from the static path",  randomPort, withApp, fetchAndOk(port => `http://localhost:${port}/assets/semantic/semantic.min.css`))
 
-    test("It should serve HTML", withApp, fetchAndOk(`http://localhost:${port}/`))
+    test("It should serve HTML", randomPort, withApp, fetchAndOk(port => `http://localhost:${port}/`))
 
-    test("It should 404 on nonexistent paths", withApp, fetchAndNotOk(`http://localhost:${port}/assets/magic/and/fooey`))
+    test("It should 404 on nonexistent paths", randomPort, withApp, fetchAndNotOk(port => `http://localhost:${port}/assets/magic/and/fooey`))
 };

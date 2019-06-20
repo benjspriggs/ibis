3e5bb7ee1c620f5bb499b70a18223fcd1ece97f7
Benjamin Spriggs <ben@sprico.com>
2019-06-19 22:42:47 -0700

    Add arguments to curried function

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ function project({
 
     const clean = gulp.parallel(cleanFolder(dist), cleanFolder(out))
 
-    const bundle = gulp.series(copyStaticSourcesToCompressed, compress)
+    const bundle = gulp.series(copyStaticSourcesToCompressed, compress(scripts, out))
 
     function package(done) {
         try {
